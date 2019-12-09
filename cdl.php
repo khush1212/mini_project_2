353283e21cc9d70a2ef1bc58dab87952f9fecfdf
prince0102 <prince.iksula@gmail.com>
2019-12-09 14:31:56 +0530

    added new changes

--- cdl.php
+++ cdl.php
@@ -1,15 +1,13 @@
 <?php require 'header.php'; ?>
-  <div class="container">
-   <br />
-   <h2 align="center">Ajax Live Data Search using Jquery PHP MySql</h2><br />
-   <div class="form-group">
-    <div class="input-group">
-     <span class="input-group-addon">Search</span>
-     <input type="text" name="search_text" id="search_text" placeholder="Search by Customer Details" class="form-control" />
+  <div class="cdl_container">
+    <div class="form-group">
+      <div class="input-group">
+        <span class="input-group-addon">Search</span>
+        <input type="text" name="search_text" id="search_text" placeholder="Search Customer Details" class="form-control" />
+      </div>
     </div>
-   </div>
-   <br />
-   <div id="result"></div>
+    <br />
+    <div id="result"></div>
   </div>
 <?php require 'footer.php'; ?>
 
