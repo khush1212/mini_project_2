f8c98cb5c3a1aec111eaaa6515badf2577bedff0
“khush1212” <khushboogu1212@gmail.com>
2019-12-09 11:15:52 +0530

    removed unwanted images from home page

--- home.php
+++ home.php
@@ -1,69 +1,7 @@
-	<?php include'header.php';?>
-		<div class="content">
-			<div class="banner_slider">
-				<img src="img/banner.jpg" />
-			</div>
-			<div class="short_banners">
-				<div class="tile_banner_tops">
-					<div class="tile_banner_front">
-						<img src="img/layout1.jpg" />
-					</div>
-					<div class="tile_banner_front">
-						<img src="img/layout2.jpg" />
-					</div>
-					<div class="tile_banner_front">
-						<img src="img/layout3.jpg" />
-					</div>
-				</div>
-				<div class="promo_short_banner">
-					<div class="tile_short_front">
-						<img src="img/layout4.jpg" />
-					</div>
-					<div class="tile_short_front">
-						<img src="img/layout5.jpg" />
-					</div>
-				</div>
-				<div class="tile_banner_jeans">
-					<div class="tile_banner_back">
-						<img src="img/layout6.jpg" />
-					</div>
-					<div class="tile_banner_back">
-						<img src="img/layout7.jpg" />
-					</div>
-					<div class="tile_banner_back">
-						<img src="img/layout8.jpg" />
-					</div>
-				</div>
-				<div class="order_info">
-					<div class="order_shipment_offer">
-						<div class="orders">
-							<span class="icons"><i class="fa fa-truck" aria-hidden="true"></i></span>
-							<div class="text">free shipping</div>
-						</div>
-						<div class="orders">
-							<span class="icons"><i class="fa fa-undo" aria-hidden="true"></i></span>
-							<div class="text">easy returns</div>
-						</div>
-						<div class="orders">
-							<span class="icons"><i class="fa fa-undo" aria-hidden="true"></i></span>
-							<div class="text">new items every day</div>
-						</div>
-					</div>
-				</div>
-				<div class="tile_banner_fit">
-					<div class="tile_banner_down">
-						<img src="img/layout9.jpg" />
-					</div>
-					<div class="tile_banner_down">
-						<img src="img/layout10.jpg" />
-					</div>
-					<div class="tile_banner_down">
-						<img src="img/layout11.jpg" />
-					</div>
-					<div class="tile_banner_down">
-						<img src="img/layout12.jpg" />
-					</div>
-				</div>
-			</div>
-		</div>
-		<?php include'footer.php';?>
+<?php include'header.php';?>
+<div class="content">
+	<div class="banner_slider">
+		<img src="img/banner.jpg" />
+	</div>
+</div>
+<?php include'footer.php';?>
